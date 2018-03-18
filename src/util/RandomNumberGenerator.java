7ad44f44c088ec6bca35e067d20be68e1c13ec06
largetimmo <kylechen49@gmail.com>
2018-03-18 13:21:34 -0400

    Initial commit

---
+++ src/util/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package util;
+
+public class RandomNumberGenerator {
+    /**
+     * Generate random 7 digit number, each digit range is 0-7
+     * @return
+     */
+    public static int GenerateRandomNumber(){
+        //TODO:
+        return -1;
+    }
+
+}
