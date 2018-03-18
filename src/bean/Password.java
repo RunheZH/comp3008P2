94ea402cbc5ed91b25f3828b791ce0092e18be16
largetimmo <kylechen49@gmail.com>
2018-03-18 16:30:16 -0400

    Add new method

--- src/bean/Password.java
+++ src/bean/Password.java
@@ -3,6 +3,13 @@ package bean;
 public class Password {
     private Scheme currentScheme;
     private int password;
+    private String password_representative;
+    /**
+     * todo:read here then goto todo#2
+     * Eg.real password -> 1234567, (octal) password_representative->1234567
+     *                            , (image) password_representative->A2C4E67
+     *                            , (binary) password_representative->10101010101010xxxxxx
+     */
     public Password(){
 
     }
@@ -20,5 +27,13 @@ public class Password {
 
     public void setPassword(int password) {
         this.password = password;
+    }
+
+    public String getPassword_representative() {
+        return password_representative;
+    }
+
+    public void setPassword_representative(String password_representative) {
+        this.password_representative = password_representative;
     }
 }
