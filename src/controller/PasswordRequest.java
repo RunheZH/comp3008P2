3941cf193e6208c6790dc07dff180fa73cc1bdfe
largetimmo <kylechen49@gmail.com>
2018-03-23 17:08:54 -0400

    Add Octal and Image scheme main logic

--- src/controller/PasswordRequest.java
+++ src/controller/PasswordRequest.java
@@ -4,43 +4,135 @@ import bean.Password;
 import bean.PwType;
 import bean.Scheme;
 import bean.User;
+import util.PasswordConvertor;
 import util.RandomNumberGenerator;
 
-import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class PasswordRequest extends BaseController {
+
+    /*
+    Type define:
+    1->ocatl
+    2->image
+    3->binary
+     */
     public String octal(HttpServletRequest req, HttpServletResponse res) {
-        Scheme scheme = Scheme.CreateOctalScheme();
-        Password password = new Password();
-        password.setCurrentScheme(scheme);
-        int password_int = RandomNumberGenerator.GenerateRandomNumber();
-        password.setPassword(password_int);
-        String target = req.getParameter("purpose");//for xxxx
-        PwType pwType = new PwType();
-        if(pwType.setType(target)==-1){
-            return "#/index.jsp?msg=Invalid Password Type";
-        }
+
         User user  = (User) req.getSession().getAttribute("user");
-        user.addPassword(pwType,password);
+        int step = Integer.parseInt(req.getSession().getAttribute("nextstep").toString());
+        Password password = null;
+        if(user.getPassword(PwType.TypeMapping.get(step))!=null){
+            //not the first time on this password
+            String target_scheme = Scheme.OCTAL;
+            password = user.getPassword(PwType.TypeMapping.get(step));
+            if(password == null){
+                //error handler
+                req.getSession().removeAttribute("user");
+                return InvalidRequestUrl;
+            }
+            if(!Scheme.verifyScheme(target_scheme)){
+                return InvalidRequestUrl;
+            }
+            //todo uncomment when passwordConvertor done
+            //password = PasswordConvertor.convertPasswordTo(password,target_scheme);
+            user.addPassword(PwType.TypeMapping.get(step),password);
+        }else{
+            //first time create password
+            String scheme = Scheme.OCTAL;
+            password = new Password();
+            password.setCurrentScheme(scheme);
+            int password_int = RandomNumberGenerator.GenerateRandomNumber();//generate new password
+            password.setPassword(password_int);
+            password.setPassword_representative(Integer.toString(password_int));
+            String step_str = req.getSession().getAttribute("nextstep").toString();
+            int step_int = Integer.parseInt(step_str);
+            String pwType = PwType.TypeMapping.get(step_int);
+            user.addPassword(pwType,password);
+        }
+        //add user to session
         req.getSession().setAttribute("user",user);
-        req.setAttribute("password",password_int);
-        /**
-         * TODO!!!
-         * Create Flow Servlet
-         * Add next method
-         */
+        req.setAttribute("password",password.getPassword_representative());
+        req.setAttribute("scheme",1);
         return "/passwordgen.jsp";
     }
 
     public String image(HttpServletRequest req, HttpServletResponse res) {
         //todo
-        return null;
+        User user = (User) req.getSession().getAttribute("user");
+        int step = Integer.parseInt(req.getSession().getAttribute("nextstep").toString());
+        Password password = user.getPassword(PwType.TypeMapping.get(step));
+        //image next step
+        //if the param does not exist -> step = 1
+        //else step = what param is
+        int image_current_step = (req.getParameter("imagens")==null?1:Integer.parseInt(req.getParameter("imagens")));
+        int image_size = 0;//how many images in the page
+        int row = 0;//high light image row
+        int col = 0;//high light image col
+        /**
+         * step->1
+         * Initial password scheme(change pwd scheme -> image)
+         * Image size 8*8
+         * Assign image row and col to request
+         * set next step to request
+         * step->2
+         * Assign image row and col to request
+         * Image size: 8*8
+         * set next step to 3
+         *
+         * step->3
+         * Image size 4*2
+         * Set next step to confirm
+         *
+         */
+        if(image_current_step == 1){
+            //password initial
+            if(password==null){
+                return InvalidRequestUrl;
+            }
+            password = PasswordConvertor.convertPasswordTo(password,Scheme.IMAGE);
+            user.addPassword(PwType.TypeMapping.get(step),password);
+            req.getSession().setAttribute("user",user);
+
+            //
+            String password_s = Integer.toString(password.getPassword());
+
+
+
+            req.setAttribute("imagens",++image_current_step);//set next step
+            row = password_s.charAt(0);
+            col = password_s.charAt(1);
+            image_size = 64;
+
+
+        }else if (image_current_step < 4){
+            assert password != null;
+            String password_s = Integer.toString(password.getPassword());
+            row = password_s.charAt((image_current_step-1)*2);
+            col = password_s.charAt((image_current_step-1)*2+1);
+
+
+        }else if (image_current_step == 4){
+            assert  password !=null;
+            String password_s = Integer.toString(password.getPassword());
+            row = Integer.parseInt(password_s.charAt(6)+"")%4;
+            col = (Integer.parseInt(password_s.charAt(6)+"")-4)%4;
+            image_size = 8;
+        }else if(image_current_step == 5){
+            return "@flow_confirm";
+        }
+        req.setAttribute("scheme",2);//set scheme
+        req.setAttribute("row",row);//set row
+        req.setAttribute("col",col);//set col
+        req.setAttribute("image_size",image_size);//set size
+        return "/passwordgen.jsp";
     }
 
     public String binary(HttpServletRequest req, HttpServletResponse res) {
         //todo
         return null;
     }
+
 }
