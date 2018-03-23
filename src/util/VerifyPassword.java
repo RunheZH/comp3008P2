094c3d7e7a2832061308ed722a31cfb1c4c83e22
largetimmo <kylechen49@gmail.com>
2018-03-23 14:03:11 -0400

    Add new method

---
+++ src/util/VerifyPassword.java
@@ -0,0 +1,16 @@
+package util;
+
+import bean.Password;
+
+public class VerifyPassword {
+    /**
+     * To verify user entered password is correct ot not
+     *
+     * @param password password object, contains original password and password representative
+     * @param user_input user entered password
+     * @return boolean
+     */
+    public static boolean verify(Password password, String user_input){
+        return false;
+    }
+}
