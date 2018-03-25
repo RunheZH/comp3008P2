1243d5c9e27a8b9ce73f6127e6d671df2b9e2406
Xiyi <liuxiyi@hotmail.com>
2018-03-25 15:04:17 -0400

    update logger

--- src/util/Logger.java
+++ src/util/Logger.java
@@ -1,26 +1,42 @@
 package util;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import bean.User;
 
 public class Logger {
+    private static FileWriter writer;
+    private static BufferedWriter bf_writer;
+
     static {
         openFile();
 
     }
     private static void openFile(){
         File file = new File("./Logging");
-        FileWriter writer = new FileWriter("./Logging", true);
-
-
 
     }
     //format: "time","userid","scheme","password type","action","something else"
     public static synchronized void writeLog(User user, String scheme, String pwtype, String action, String data){
+        try {
+            writer = new FileWriter("./Logging", true);
+            bf_writer = new BufferedWriter(writer);
+            bf_writer.write("Time: " + System.currentTimeMillis());
+            bf_writer.write("UserId: " + user.getUserid());
+            bf_writer.write("Scheme: " + scheme);
+            bf_writer.write("PasswordType: " + pwtype);
+            bf_writer.write("Action: " + action);
+            bf_writer.write("Data: " + data + "\n");
 
-
-        System.currentTimeMillis();
-
+        }catch (IOException e){
+            System.err.println(e.getMessage());
+        }finally {
+            if(bf_writer!=null){
+                bf_writer.close();
+            }
+        }
     }
 
 
